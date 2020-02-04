2c57c3e3100b5713f9013d7abb1414457d13be41
kamiyagunji <kamiyagunji@icloud.com>
2020-02-04 10:17:52 +0900

    ソースコード追加

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBOutlet weak var countUpLabel: UILabel!
+    
+     var count = 0
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         countUpLabel.text = "0"
+     }
 
+     @IBAction func plus(_ sender: Any) {
+         // カウントアップ
+         count = count + 1
+         // 反映させる(キャストで文字列型に変換させる)
+         countUpLabel.text = String(count)
+         // 数字が10以上になったら、色を黄色に変更させる
+         if count >= 10 {
+             changeTextColor()
+         }
+     }
+
+     @IBAction func minus(_ sender: Any) {
+         // カウントダウン
+         count = count - 1
+         // 反映させる(キャストで文字列型に変換させる)
+         countUpLabel.text = String(count)
+         // 数字が0以下になったら、色を白に変更させる
+         if count <= 0 {
+             resetColor()
+         }
+     }
+
+     // 文字の色を変えるメソッドを追加
+     func changeTextColor(){
+         countUpLabel.textColor = .yellow
+     }
+     func resetColor(){
+         countUpLabel.textColor = .white
+     }
 }
 
